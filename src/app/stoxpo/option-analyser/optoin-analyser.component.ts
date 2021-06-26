828fadd6385f4661809039bf554d75866076f306
naughtycodes <krish_nan@outlook.com>
2021-06-26 18:10:48 +0530

    added current next and last options data

--- src/app/stoxpo/option-analyser/optoin-analyser.component.ts
+++ src/app/stoxpo/option-analyser/optoin-analyser.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
@@ -29,10 +30,13 @@ export class OptionAnalyserComponent implements OnInit {
   activeTabIndex: number;
   changeFlag: boolean = false;
   firstCounter: boolean = true;
+  isCurrentMonthData: boolean = false;
   isNextMonthData: boolean = true;
   isLastMonthData: boolean = false;
   isOptInfo: boolean = false;
-  isAllFundsTab: boolean = false;
+  isAllFundsTab: boolean = false;  
+  fileName: any;
+  fetchMonthKey: string = 'NEXT';
 
   defaultColDef = {
     width: screen.width <= 450 ? 80 : 120,
@@ -115,23 +119,6 @@ export class OptionAnalyserComponent implements OnInit {
           comparator: numberSort,
         },
         {
-          field: 'volume',
-          headerName: 'VOL',
-          filter: 'agTextColumnFilter',
-          floatingFilter: true,
-          width: 80,
-          comparator: numberSort,
-        }, 
-        {
-          field: 'rsi',
-          headerName: 'RSI',
-          filter: 'agTextColumnFilter',
-          wrapText: true,
-          autoHeight: true,
-          floatingFilter: true,
-          comparator: numberSort,
-        },
-        {
           headerName: 'PRE',
           field: 'premium',
           filter: 'agTextColumnFilter',
@@ -147,6 +134,50 @@ export class OptionAnalyserComponent implements OnInit {
           wrapText: true,
           autoHeight: true,
           floatingFilter: true,
+          comparator: numberSort,
+        },
+        {
+          field: 'rsi',
+          headerName: 'RSI',
+          filter: 'agTextColumnFilter',
+          wrapText: true,
+          autoHeight: true,
+          floatingFilter: true,
+          comparator: numberSort,
+        }, 
+        {
+          headerName: 'LOT-SZ',
+          field: 'lotSize',
+          filter: 'agTextColumnFilter',
+          wrapText: true,
+          autoHeight: true,
+          floatingFilter: true,
+          comparator: numberSort,
+        },
+        {
+          headerName: 'EX-DD',
+          field: 'daysToExpiry',
+          filter: 'agTextColumnFilter',
+          wrapText: true,
+          autoHeight: true,
+          floatingFilter: true,
+          comparator: numberSort,
+        },
+        {
+          headerName: 'AMT-(L)',
+          field: 'amtReqToCover',
+          filter: 'agTextColumnFilter',
+          wrapText: true,
+          autoHeight: true,
+          floatingFilter: true,
+          comparator: numberSort,
+        },
+        {
+          field: 'volume',
+          headerName: 'VOL',
+          filter: 'agTextColumnFilter',
+          floatingFilter: true,
+          width: 80,
           comparator: numberSort,
         }, 
         {
@@ -164,42 +195,8 @@ export class OptionAnalyserComponent implements OnInit {
     }
 
   ngOnInit(): void { 
-
     this.primengConfig.ripple = true;
-
-    this.fetchOptionsService.getOptions().subscribe((d:any) => {
-      for(const o in d){
-        for(const p in d[o]){
-          if(p === 'optionChain' && o !== ''){
-            const c = d[o][p];
-            from(c).pipe(map((e:any) => {
-                if(e.poVolume > 0){
-                  let price = d[o].stPrice.lastPrice.replace(',','');
-                  return {
-                    optionChain: o,
-                    strikePrice: e.strikePrice,
-                    rsi: parseFloat(d[o].rsi.value).toFixed(2),
-                    premium: e.poLTP,
-                    volume: e.poVolume,
-                    precentage: (e.poLTP/(e.strikePrice/100)).toFixed(2),
-                    expiry: d[o].expiryDate,
-                    lastPrice: price,
-                    companyName: d[o].stPrice.companyName,
-                    diffPercentage: ( (price - e.strikePrice) / (price/100) ).toFixed(2) 
-                  }
-                }
-            })).subscribe(
-              (v: any) => {
-                if(v !== undefined){
-                  this.t.push(v); 
-                }
-              }
-            );
-          }
-        }
-      } 
-      this.rowData = this.t;
-    });
+    this.loadOptionsData(this.fetchMonthKey);
   }
 
   onGridReady(params: any) {
@@ -218,16 +215,110 @@ export class OptionAnalyserComponent implements OnInit {
     //console.log('changing tab:'+$event.originalEvent.currentTarget.innerText);
     let tabIndex = parseInt($event.index);
     if(tabIndex === 0){
-      this.isNextMonthData = true;
+      this.isCurrentMonthData = true;
+      this.fetchMonthKey = 'CURRENT';
+      this.loadOptionsData(this.fetchMonthKey);
     } else if(tabIndex === 1){
       this.isLastMonthData = true;
+      this.fetchMonthKey = 'NEXT';
+      this.loadOptionsData(this.fetchMonthKey);
     } else if(tabIndex === 2){
-      this.isAllFundsTab = true;
+      this.isLastMonthData = true;
+      this.fetchMonthKey = 'LAST';
+      this.loadOptionsData(this.fetchMonthKey);
     } else if(tabIndex === 3){
       this.isOptInfo = true;
     } else {
       this.isNextMonthData = true;
+      this.fetchMonthKey = 'NEXT';
+      this.loadOptionsData(this.fetchMonthKey);
     }
+  }
+
+  daysToExpiry(date: any){
+
+    let dd = date.substring(0,2);
+    let yyyy = date.substring(5,9);
+    let mm = 1;
+
+    if(date.substring(2,5).toUpperCase() === 'JAN') {
+      mm = 1;
+    } else if(date.substring(2,5).toUpperCase() === 'FEB') {
+      mm = 2;
+    } else if(date.substring(2,5).toUpperCase() === 'MAR') {
+      mm = 3;
+    } else if(date.substring(2,5).toUpperCase() === 'APR') {
+      mm = 4;
+    } else if(date.substring(2,5).toUpperCase() === 'MAY') {
+      mm = 5;
+    } else if(date.substring(2,5).toUpperCase() === 'JUN') {
+      mm = 6;
+    } else if(date.substring(2,5).toUpperCase() === 'JUL') {
+      mm = 7;
+    } else if(date.substring(2,5).toUpperCase() === 'AUG') {
+      mm = 8;
+    } else if(date.substring(2,5).toUpperCase() === 'SEP') {
+      mm = 9;
+    } else if(date.substring(2,5).toUpperCase() === 'OCT') {
+      mm = 10;
+    } else if(date.substring(2,5).toUpperCase() === 'NOV') {
+      mm = 11;
+    } else {
+      mm = 12;
+    }
+
+    let diff = (new Date(mm+'/'+dd+'/'+yyyy).getTime() - new Date().getTime()) / (1000 * 3600 * 24);
+
+    return diff.toFixed(0)+' days';
+  }
+
+  loadOptionsData(fetchMonthKey: string) {
+    this.rowData = [];
+    this.t = [];
+    this.fetchOptionsService.getOptions(fetchMonthKey).subscribe((d:any) => {
+      for(const o in d){
+        for(const p in d[o]){
+          if(p === 'optionChain' && o !== ''){
+            const c = d[o][p];
+            from(c).pipe(map((e:any) => {
+                if(parseInt(e.poVolume) > 0){
+                  let price = d[o].stPrice.lastPrice.replace(',','');
+                  return {
+                    optionChain: o,
+                    strikePrice: e.strikePrice,
+                    rsi: parseFloat(d[o].rsi.value).toFixed(2),
+                    premium: e.poLTP,
+                    volume: e.poVolume,
+                    lotSize: d[o].lotSize,
+                    daysToExpiry: this.daysToExpiry(d[o].expiryDate),
+                    amtReq: d[o].lotSize*e.strikePrice,
+                    amtReqToCover: (d[o].lotSize*price/100000).toFixed(2),
+                    precentage: (e.poLTP/(e.strikePrice/100)).toFixed(2),
+                    expiry: d[o].expiryDate,
+                    lastPrice: price,
+                    companyName: d[o].stPrice.companyName,
+                    diffPercentage: ( (price - e.strikePrice) / (price/100) ).toFixed(2) 
+                  }
+                }
+            })).subscribe(
+              (v: any) => {
+                if(v !== undefined){
+                  this.t.push(v); 
+                }
+              }
+            );
+          }
+        }
+      } 
+      this.rowData = this.t;
+      if(screen.width <= 450 ){
+        this.gridApi.setHeaderHeight(20);
+      } else {
+        this.gridApi.setHeaderHeight();
+        this.gridApi.sizeColumnsToFit();
+      }
+    });
+
   }
 
 }
--- src/app/stoxpo/services/fetch-options.service.ts
+++ src/app/stoxpo/services/fetch-options.service.ts
@@ -8,13 +8,74 @@ import { Observable } from 'rxjs';
 })
 export class FetchOptionsService {
 
-  public static getOptions = 'https://raw.githubusercontent.com/hellomohanakrishnan/NseOptionsChainData/main/LastUpdatedData.json';
+  public static getOptions = 'https://raw.githubusercontent.com/hellomohanakrishnan/NseOptionsChainData/main/';
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  public getOptions() {
+  public getOptions(fetchMonthKey: any) {
     const headers= new HttpHeaders();
-    return this.http.get(FetchOptionsService.getOptions, { 'headers': headers });
+    return this.http.get(FetchOptionsService.getOptions+this.getMonthToFetch(fetchMonthKey)+'_UpdatedData.json', { 'headers': headers });
   }
+
+  getMonthToFetch(fetchMonthKey: string) {
+    const MMM = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+    let mm = MMM[new Date().getMonth()];
+
+    if(fetchMonthKey === 'CURRENT') {
+      return mm = new Date().getDate() <= this.getLastThrusday(new Date()) ? MMM[new Date().getMonth()] : MMM[new Date().getMonth()+1];
+    } else if(fetchMonthKey === 'NEXT') {
+      return mm = new Date().getDate() <= this.getLastThrusday(new Date()) ? MMM[new Date().getMonth()+1] : MMM[new Date().getMonth()+2];
+    } else if(fetchMonthKey === 'LAST') {
+      return mm = new Date().getDate() <= this.getLastThrusday(new Date()) ? MMM[new Date().getMonth()+2] : MMM[new Date().getMonth()+3];
+    } else {
+      return mm = new Date().getDate() <= this.getLastThrusday(new Date()) ? MMM[new Date().getMonth()] : MMM[new Date().getMonth()+1];
+    }
+
+  }
+
+  getLastThrusday(date: Date) {
+    let dd = date.getDate();
+    let th = 0;
+    switch (date.getMonth()) {
+      case 0:
+      case 2:
+      case 4:
+      case 6:
+      case 7:
+      case 9:
+      case 11:
+        th = new Date(date.getMonth()+1+'/31/'+date.getFullYear()).getDay();
+        if(th > 4) {
+          dd=new Date(date.getMonth()+1+'/'+(31-(th-4))+'/'+date.getFullYear()).getDate();
+        } else if(th < 4) {
+          dd=new Date(date.getMonth()+1+'/'+(31-(4+th))+'/'+date.getFullYear()).getDate()+1;
+        } else {
+          dd=new Date(date.getMonth()+1+'/'+(31)+'/'+date.getFullYear()).getDate();
+        }
+      break;
+      case 1:
+        let febDays = date.getFullYear() % 4 === 0 ? 29 : 28;
+        th = new Date(date.getMonth()+1+'/'+febDays+'/'+date.getFullYear()).getDay();
+        if(th > 4) {
+          dd=new Date(date.getMonth()+1+'/'+(febDays-(th-4))+'/'+date.getFullYear()).getDate();
+        } else if(th < 4) {
+          dd=new Date(date.getMonth()+1+'/'+(febDays-(4+th))+'/'+date.getFullYear()).getDate()+1;
+        } else {
+          dd=new Date(date.getMonth()+1+'/'+(febDays-(4))+'/'+date.getFullYear()).getDate();
+        }
+        break;
+        default:
+          th = new Date(date.getMonth()+1+'/30/'+date.getFullYear()).getDay();
+          if(th > 4) {
+            dd=new Date(date.getMonth()+1+'/'+(30-(th-4))+'/'+date.getFullYear()).getDate();
+          } else if(th < 4) {
+            dd=new Date(date.getMonth()+1+'/'+(30-(4+th))+'/'+date.getFullYear()).getDate()+1;
+          } else {
+            dd=new Date(date.getMonth()+1+'/'+(30)+'/'+date.getFullYear()).getDate();
+          }
+      }
+      return dd;
+    }
+
 
 }
